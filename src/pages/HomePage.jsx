1b6429fc9b1eda0a7c5eafffb46f4af404ca0e18
gabriel dutra lagares <gabriel.lagares20@gmail.com>
2024-06-06 14:15:02 -0300

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,31 @@
+import { Container, Row, Col } from "react-bootstrap";
+import HeroImage from "../images/capa.png";
+
 const HomePage = () => {
   return (
     <div className="homepage">
-      <header className="w-100 min-vh-100"></header>
-        <div className="modalidade w-100 min-vh-100"></div>
+      <header className="w-100 min-vh-100 d-flex align-items-center">
+      <Container>
+        <Row className="header-box d-flex align-items-center">
+          <Col lg="6" className="col">
+            <h1 className="mb-4">
+              <span>Mergulhe</span> em uma jornada <br/> de saúde, bem-estar e
+              diversão!
+            </h1>
+            <p className="mb-4">
+            Bem-vindo à Academia Nado Livre, onde a saúde e o bem-estar fluem juntos! Nossa missão é proporcionar um ambiente acolhedor e motivador para que você possa alcançar seus objetivos, seja melhorar suas habilidades na água ou desfrutar dos benefícios de um exercício de baixo impacto. Vamos nadar e nos exercitar juntos rumo a uma vida mais saudável e feliz!
+            </p>
+            <button className="btn btn-outline-danger round-1"> Saiba mais</button>
+          </Col>
+          <Col lg="6" className="pt-lg-0 pt-5">
+          <img src={HeroImage} alt="hero-img" />
+          </Col>
+        </Row>
+      </Container>
+      </header>
+      <div className="modalidade w-100 min-vh-100"></div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
