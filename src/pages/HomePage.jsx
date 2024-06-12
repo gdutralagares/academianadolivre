32ef53ea4f4cf69c9b912d7822a0ce667ff4687b
gabriel dutra lagares <gabriel.lagares20@gmail.com>
2024-06-12 13:45:56 -0300

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -40,7 +40,7 @@ const HomePage = () => {
               </button>
             </Col>
             <Col lg="6" className="pt-lg-0 pt-5">
-              {/* <img src={Capa} alt="" /> */}
+              <img src={Capa} alt="" />
             </Col>
           </Row>
         </Container>
