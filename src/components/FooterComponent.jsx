e24db7eb41a2abcb05ade0bbef90424d44112224
gabriel dutra lagares <gabriel.lagares20@gmail.com>
2024-06-12 16:18:56 -0300

    footer

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,39 +1,31 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Logo from "../images/logo-removed.png";
 
 const FooterComponent = () => {
   return (
-    <div>
+    <div className="footer">
       <Container>
         <Row>
           <Col>
-            <h3 className="fw-bold">Academia Nado Livre</h3>
-            <p className="desc">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-              possimus doloribus explicabo ipsa, amet perferendis?
-            </p>
+          <img src={Logo} alt="" className="logo" />
+            <p className="desc">A mais de 30 anos promovendo a saúde!</p>
             <div className="no">
               <Link className="text-decoration-none">
                 <i className="fa-brands fa-whatsapp"></i>
                 <p className="m-0">(62) 981532838</p>
               </Link>
             </div>
-            <div className="mail">
-              <Link className="text-decoration-none">
-                <i className="fa-regular fa-envelope"></i>
-                <p className="m-0">academianadolivre@yahoo.com.br</p>
-              </Link>
+            <div className="social">
+              <div className="mail">
+                <Link>
+                  <i className="fa-brands fa-instagram"></i>
+                  <i className="fa-regular fa-envelope"></i>
+                  <i className="fa-brands fa-facebook"></i>
+                </Link>
+              </div>
             </div>
           </Col>
-          <Col className="d-flex flex-column col-lg-2 col">
-          <h5 className="fw-bold">Menu</h5>
-          <Link to="">Home</Link>
-          <Link to="modalidade">Modalidades</Link>
-          <Link to="fotos">Fotos</Link>
-          <Link to="faq">Faq</Link>
-          <Link to="contato">Contato</Link>
-          </Col>
-          <Col></Col>
         </Row>
       </Container>
     </div>
