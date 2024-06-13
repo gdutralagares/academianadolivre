af1dfb1d072f041a8ee8577106e11f63a228dc0b
gabriel dutra lagares <gabriel.lagares20@gmail.com>
2024-06-13 13:08:08 -0300

    update

--- src/data/index.js
+++ src/data/index.js
@@ -3,10 +3,11 @@ import Image2 from "/src/images/baby.png";
 import Image3 from "/src/images/hidro.png";
 
 import Foto1 from "/src/images/foto1.jpeg";
-import Foto2 from "/src/images/foto2.jpeg";
-import Foto3 from "/src/images/foto1.jpeg";
-import Foto4 from "/src/images/foto3.jpeg";
+import Foto2 from "/src/images/foto4.jpeg";
+import Foto3 from "/src/images/foto5.jpeg";
+import Foto4 from "/src/images/foto6.jpeg";
 import Foto5 from "/src/images/foto2.jpeg";
+import Foto6 from "/src/images/foto3.jpeg";
 
 export const navLinks = [
   {
@@ -36,39 +37,12 @@ export const navLinks = [
   },
 ];
 
-export const semuaKelas = [
-  {
-    id: 1,
-    image: Image1,
-    title: "Natação",
-    price: "Rp. 300.000",
-    buy: "Beli Kelas",
-    delay: "1300",
-  },
-  {
-    id: 2,
-    image: Image2,
-    title: "UI UX Website Design for Beginner with Figma",
-    price: "Rp. 340.000",
-    buy: "Beli Kelas",
-    delay: "1600",
-  },
-  {
-    id: 3,
-    image: Image3,
-    title: "Mastering Web Developer Bootstrap 5",
-    price: "Rp. 280.000",
-    buy: "Beli Kelas",
-    delay: "1900",
-  },
-];
 
 export const atividades = [
   {
     id: 1,
     image: Image1,
     title: "Natação",
-    price: "Rp. 300.000",
     mais: "Saiba mais",
     text: "A natação é um esporte aquático que combina técnica, resistência e coordenação, proporcionando um exercício completo que fortalece os músculos, melhora a capacidade cardiovascular e promove o bem-estar geral.",
     delay: "",
@@ -77,7 +51,6 @@ export const atividades = [
     id: 2,
     image: Image2,
     title: "Natação para bebês",
-    price: "Rp. 340.000",
     mais: "Saiba mais",
     text:  "Natação para bebês é uma atividade segura e divertida que introduz os pequenos ao ambiente aquático, ajudando no desenvolvimento motor, fortalecimento muscular e na criação de um vínculo especial com os pais.",
     delay: "200",
@@ -86,7 +59,6 @@ export const atividades = [
     id: 3,
     image: Image3,
     title: "Hidroginástica",
-    price: "Rp. 280.000",
     mais: "Saiba mais",
     text: "Hidroginástica é uma forma de exercício realizada em água rasa, combinando movimentos aeróbicos e de resistência que oferecem benefícios como fortalecimento muscular, melhoria da flexibilidade e baixo impacto nas articulações.",
     delay: "400",
@@ -98,29 +70,35 @@ export const fotos = [
   {
     id: 1,
     image: Foto1,
-    name: "People 1",
+    name: "",
   },
   {
     id: 2,
     image: Foto2,
-    name: "People 2",
+    name: "",
 
   },
   {
     id: 3,
     image: Foto3,
-    name: "People 3",
+    name: "",
+  },
+  {
+    id: 4,
+    image: Foto4,
+    name: "",
   },
   {
     id: 5,
     image: Foto5,
-    name: "People 3",
+    name: "",
   },
   {
-    id: 5,
-    image: Foto5,
-    name: "People 3",
+    id: 6,
+    image: Foto6,
+    name: "",
   },
+
 
   
 ];
@@ -128,38 +106,38 @@ export const fotos = [
 export const dataSwiper = [
   {
     id: 1,
-    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor, saepe a. Quod eos non, dolores sapiente dicta quam esse reprehenderit explicabo vitae nesciunt ut laborum harum, vel optio corporis voluptatem?",
     image: Foto1,
     name: "People 1",
-    skill: "UI UX Designer",
   },
   {
     id: 2,
-    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor, saepe a. Quod eos non, dolores sapiente dicta quam esse reprehenderit explicabo vitae nesciunt ut laborum harum, vel optio corporis voluptatem?",
     image: Foto2,
     name: "People 2",
-    skill: "Flutter Developer",
+
   },
   {
     id: 3,
-    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor, saepe a. Quod eos non, dolores sapiente dicta quam esse reprehenderit explicabo vitae nesciunt ut laborum harum, vel optio corporis voluptatem?",
     image: Foto3,
     name: "People 3",
-    skill: "Web Developer",
   },
   {
     id: 4,
-    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor, saepe a. Quod eos non, dolores sapiente dicta quam esse reprehenderit explicabo vitae nesciunt ut laborum harum, vel optio corporis voluptatem?",
     image: Foto4,
     name: "People 3",
-    skill: "Web Developer",
+
   },
   {
     id: 5,
-    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor, saepe a. Quod eos non, dolores sapiente dicta quam esse reprehenderit explicabo vitae nesciunt ut laborum harum, vel optio corporis voluptatem?",
+
     image: Foto5,
     name: "People 3",
-    skill: "Web Developer",
+
+  },
+  {
+    id: 6,
+    image: Foto6,
+    name: "People 3",
+
   },
 ];
 
